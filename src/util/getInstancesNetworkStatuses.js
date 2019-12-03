700b83bb9054de933b8a2cb8e7b05f756c9f42c3
deliciousmonster <jaxon@deliciousmonster.com>
2019-12-03 11:23:42 -0700

    Update getInstancesNetworkStatuses.js

--- src/util/getInstancesNetworkStatuses.js
+++ src/util/getInstancesNetworkStatuses.js
@@ -3,8 +3,8 @@ const getInstanceNetworkStatus = async (instance, queryHarperDB) => {
   return {
     ...instance,
     fabric: {
-      inbound_connections: inbound_connections.filter((c) => c.host_address.indexOf('::ffff') === -1),
-      outbound_connections: outbound_connections.filter((c) => c.host_address.indexOf('::ffff') === -1),
+      inbound_connections: inbound_connections ? inbound_connections.filter((c) => c.host_address.indexOf('::ffff') === -1) : [],
+      outbound_connections: outbound_connections ? outbound_connections.filter((c) => c.host_address.indexOf('::ffff') === -1) : [],
       name: Object.keys(__originator)[0],
     },
   };
