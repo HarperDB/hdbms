029ebc210d013bff065507579440fc7ae1d98400
deliciousmonster <jaxon@deliciousmonster.com>
2020-03-12 16:55:28 -0600

    comment out sign up

--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -107,12 +107,14 @@ export default () => {
             </div>
           ) : (
             <Row className="text-small">
-              <Col xs="6" className="text-nowrap">
+              <Col xs="12" className="text-nowrap text-center">
                 <NavLink to="/forgot-password" className="login-nav-link">Forgot Password</NavLink>
               </Col>
+              {/*
               <Col xs="6" className="text-nowrap text-right">
                 <NavLink to="/sign-up" className="login-nav-link">Sign Up for Free</NavLink>
               </Col>
+              */}
             </Row>
           )}
         </>
