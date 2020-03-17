f704bc3538530b7f64695cb59669ac0fa652d76c
deliciousmonster <jaxon@deliciousmonster.com>
2020-03-17 14:48:58 -0600

    API url from config file

--- src/api/queryLMS.js
+++ src/api/queryLMS.js
@@ -1,9 +1,11 @@
+import config from '../../config';
+
 export default async ({ endpoint, payload, auth }) => {
   console.log('Querying LMS API', endpoint);
 
   try {
     const request = await fetch(
-      `https://dfu2a1toa9.execute-api.us-west-2.amazonaws.com/Prod/${endpoint}`,
+      `${config.lms_api_url}${endpoint}`,
       {
         method: 'POST',
         body: JSON.stringify(payload),
