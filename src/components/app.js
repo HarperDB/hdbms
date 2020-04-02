e61d8699be11ed701b95056609d6441e8232f3b3
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-02 15:07:27 -0600

    coming soon only on app

--- src/components/app.js
+++ src/components/app.js
@@ -14,20 +14,23 @@ import ProtectedRoute from './shared/protectedRoute';
 import Account from './account';
 import Instances from './instances';
 import Instance from './instance';
+import ComingSoon from './comingSoon';
+
+const showComingSoon = window.location.host === 'studio.harperdb.io';
 
 export default () => (
   <div id="app-container">
     <Switch>
-      <Route component={SignIn} exact path="/sign-in" />
-      <Route component={SignUp} exact path="/sign-up" />
+      <Route component={showComingSoon ? ComingSoon : SignIn} exact path="/sign-in" />
+      <Route component={showComingSoon ? ComingSoon : SignUp} exact path="/sign-up" />
       <Route component={SignUpStandalone} exact path="/sign-up-standalone" />
       <Route component={Pricing} exact path="/pricing" />
-      <Route component={UpdatePassword} exact path="/update-password" />
-      <Route component={ResetPassword} exact path="/reset-password" />
-      <Route component={ResendRegistrationEmail} exact path="/resend-registration-email" />
-      <ProtectedRoute component={Instance} path="/instance/:compute_stack_id" />
-      <ProtectedRoute component={Account} path="/account/:view?" />
-      <ProtectedRoute component={Instances} path="/instances/:action?/:purchaseStep?" />
+      <Route component={showComingSoon ? ComingSoon : UpdatePassword} exact path="/update-password" />
+      <Route component={showComingSoon ? ComingSoon : ResetPassword} exact path="/reset-password" />
+      <Route component={showComingSoon ? ComingSoon : ResendRegistrationEmail} exact path="/resend-registration-email" />
+      <ProtectedRoute component={showComingSoon ? ComingSoon : Instance} path="/instance/:compute_stack_id" />
+      <ProtectedRoute component={showComingSoon ? ComingSoon : Account} path="/account/:view?" />
+      <ProtectedRoute component={showComingSoon ? ComingSoon : Instances} path="/instances/:action?/:purchaseStep?" />
       <Redirect to="/sign-in" />
     </Switch>
   </div>
