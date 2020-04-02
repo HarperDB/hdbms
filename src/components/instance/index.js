aab8399896ceed71921ea9f9686b2eb1c1ad1506
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-02 13:03:14 -0600

    cleaner

--- src/components/instance/index.js
+++ src/components/instance/index.js
@@ -22,40 +22,36 @@ export default () => {
   }));
 
   const refreshInstance = async () => {
-    const auth = instanceAuths[compute_stack_id];
-    const thisInstance = instances.find((i) => i.compute_stack_id === compute_stack_id);
-    const compute = products[thisInstance.is_local ? 'localCompute' : 'cloudCompute'].find((p) => p.value === thisInstance.stripe_plan_id);
-    const storage = thisInstance.is_local ? null : products.cloudStorage.find((p) => p.value === thisInstance.data_volume_size);
-    const computeProducts = thisInstance.is_local ? products.localCompute : products.cloudCompute;
-    const storageProducts = thisInstance.is_local ? false : products.cloudStorage;
-    await buildActiveInstanceObject({
-      thisInstance,
-      auth,
-      compute,
-      storage,
-      computeProducts,
-      storageProducts,
-    });
-    setLoadingInstance(false);
+    if (instanceAuths && compute_stack_id && instanceAuths[compute_stack_id] && products && regions) {
+      setLoadingInstance(true);
+      const auth = instanceAuths[compute_stack_id];
+      const thisInstance = instances.find((i) => i.compute_stack_id === compute_stack_id);
+      const compute = products[thisInstance.is_local ? 'localCompute' : 'cloudCompute'].find((p) => p.value === thisInstance.stripe_plan_id);
+      const storage = thisInstance.is_local ? null : products.cloudStorage.find((p) => p.value === thisInstance.data_volume_size);
+      const computeProducts = thisInstance.is_local ? products.localCompute : products.cloudCompute;
+      const storageProducts = thisInstance.is_local ? false : products.cloudStorage;
+      await buildActiveInstanceObject({
+        thisInstance,
+        auth,
+        compute,
+        storage,
+        computeProducts,
+        storageProducts,
+      });
+      setLoadingInstance(false);
+    }
   };
 
   useEffect(() => {
     const cancelSub = instanceState.subscribe(
       (s) => s.lastUpdate,
-      async () => {
-        if (instanceAuths && compute_stack_id && instanceAuths[compute_stack_id] && products && regions) {
-          refreshInstance();
-        }
-      }
+      () => refreshInstance()
     );
     return () => cancelSub();
   }, []);
 
   useEffect(() => {
-    if (instanceAuths && compute_stack_id && instanceAuths[compute_stack_id] && products && regions) {
-      setLoadingInstance(true);
-      refreshInstance();
-    }
+    refreshInstance();
   }, [compute_stack_id]);
 
   return (
