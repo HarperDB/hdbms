4f7144904b140d33ed1b7a2bf163f13c72943521
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-06 15:47:05 -0600

    console log

--- src/util/instance/handleSchemaTableRedirect.js
+++ src/util/instance/handleSchemaTableRedirect.js
@@ -1,6 +1,4 @@
 export default ({ entities, compute_stack_id, schema, table, history, targetPath }) => {
-  console.log(entities, schema, table);
-
   switch (true) {
     case !entities.schemas.length && history.location.pathname !== targetPath:
       history.push(`/instance/${compute_stack_id}${targetPath}`);
