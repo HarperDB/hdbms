d407d81c011048c8284c2d7ba528c132b57e85db
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-08 11:07:31 -0600

    turn it on

--- src/components/app.js
+++ src/components/app.js
@@ -14,37 +14,25 @@ import ProtectedRoute from './shared/protectedRoute';
 import Account from './account';
 import Instances from './instances';
 import Instance from './instance';
-import ComingSoon from './comingSoon';
 
-const showComingSoon = window.location.host === 'studio.harperdb.io';
-
-export default () =>
-  showComingSoon ? (
-    <div id="app-container">
-      <Switch>
-        <Route component={SignUpStandalone} exact path="/sign-up-standalone" />
-        <Route component={Pricing} exact path="/pricing" />
-        <Route component={ComingSoon} path="/" />
-      </Switch>
-    </div>
-  ) : (
-    <div id="app-container">
-      <Switch>
-        <Route component={SignIn} exact path="/sign-in" />
-        <Route component={SignUp} exact path="/sign-up" />
-        <Route component={SignUpStandalone} exact path="/sign-up-standalone" />
-        <Route component={Pricing} exact path="/pricing" />
-        <Route component={UpdatePassword} exact path="/update-password" />
-        <Route component={ResetPassword} exact path="/reset-password" />
-        <Route component={ResendRegistrationEmail} exact path="/resend-registration-email" />
-        <ProtectedRoute>
-          <Switch>
-            <Route component={Instance} path="/instance/:compute_stack_id" />
-            <Route component={Account} path="/account/:view?" />
-            <Route component={Instances} path="/instances/:action?/:purchaseStep?" />
-          </Switch>
-        </ProtectedRoute>
-        <Redirect to="/sign-in" />
-      </Switch>
-    </div>
-  );
+export default () => (
+  <div id="app-container">
+    <Switch>
+      <Route component={SignIn} exact path="/sign-in" />
+      <Route component={SignUp} exact path="/sign-up" />
+      <Route component={SignUpStandalone} exact path="/sign-up-standalone" />
+      <Route component={Pricing} exact path="/pricing" />
+      <Route component={UpdatePassword} exact path="/update-password" />
+      <Route component={ResetPassword} exact path="/reset-password" />
+      <Route component={ResendRegistrationEmail} exact path="/resend-registration-email" />
+      <ProtectedRoute>
+        <Switch>
+          <Route component={Instance} path="/instance/:compute_stack_id" />
+          <Route component={Account} path="/account/:view?" />
+          <Route component={Instances} path="/instances/:action?/:purchaseStep?" />
+        </Switch>
+      </ProtectedRoute>
+      <Redirect to="/sign-in" />
+    </Switch>
+  </div>
+);
