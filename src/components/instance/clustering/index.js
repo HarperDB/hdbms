a0df4bf343588535840a42b8c89fffa9d0bb7d86
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-13 10:38:53 -0600

    debug clustering issue

--- src/components/instance/clustering/index.js
+++ src/components/instance/clustering/index.js
@@ -11,6 +11,12 @@ export default () => {
   const { compute_stack_id } = useParams();
   const network = useStoreState(instanceState, (s) => s.network, [compute_stack_id]);
 
+  console.log('network', !!network);
+  console.log('is_enabled', !!network.is_enabled);
+  console.log('cluster_user', !!network.cluster_user);
+  console.log('cluster_role', !!network.cluster_role);
+  console.log('node_name', network.name === compute_stack_id);
+
   return !network ? (
     <i className="fa fa-spinner fa-spin text-white" />
   ) : network.is_enabled && network.cluster_user && network.cluster_role && network.name === compute_stack_id ? (
