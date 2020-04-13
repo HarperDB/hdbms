c92ae414bc07b808e5a5f1010d72d5b3651bbb3a
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-13 10:17:16 -0600

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,12 @@
+export default {
+  stripe_public_key: 'pk_test_replacethiswithyourtestkey',
+  lms_api_url: 'https://dev.harperdbcloudservices.com/',
+  tc_version: '2020-01-01',
+  instances_refresh_rate: 10000,
+  instance_refresh_rate: 5000,
+  total_cloud_instance_limit: 10,
+  free_cloud_instance_limit: 1,
+  total_local_instance_limit: false,
+  free_local_instance_limit: false,
+  max_file_upload_size: 10380902,
+};
