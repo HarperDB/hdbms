954ceacd617ed410f80d4e890b32e46a316f45df
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-21 17:39:04 -0600

    coupon in billing

---
+++ src/api/lms/addCoupon.js
@@ -0,0 +1,12 @@
+import queryLMS from '../queryLMS';
+
+export default async ({ auth, customer_id, coupon_code }) => {
+  const response = await queryLMS({
+    endpoint: 'addCoupon',
+    method: 'POST',
+    auth,
+    payload: { customer_id, coupon_code },
+  });
+
+  return response.body;
+};
--- src/components/account/billing/index.js
+++ src/components/account/billing/index.js
@@ -3,6 +3,7 @@ import { Row, Col, Card, CardBody } from '@nio/ui-kit';
 
 import CreditCard from './card';
 import Invoices from './invoices';
+import Coupons from './coupons';
 
 export default () => (
   <Row>
@@ -15,7 +16,13 @@ export default () => (
       </Card>
     </Col>
     <Col md="6" className="mb-4">
-      <span className="text-white mb-2 floating-card-header">invoices</span>
+      <span className="text-white mb-2 floating-card-header">coupons</span>
+      <Card className="mt-3 mb-4">
+        <CardBody>
+          <Coupons />
+        </CardBody>
+      </Card>
+      <span className="text-white my-3 floating-card-header">invoices</span>
       <Card className="my-3">
         <CardBody>
           <Invoices />
