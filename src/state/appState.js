ae52266b7df742e4f6aae9848f6406e62318ab09
deliciousmonster <jaxon@deliciousmonster.com>
2020-04-23 11:19:41 -0600

    handle response

--- src/state/appState.js
+++ src/state/appState.js
@@ -12,7 +12,7 @@ const appState = new Store({
   filterSearch: '',
   filterLocal: true,
   filterCloud: true,
-  version: { number: '2.0.37-BETA', location: 'https://registry.npmjs.org/harperdb/-/harperdb-2.0.37-BETA.tgz' },
+  version: { number: '...', location: '#' },
 });
 
 export default appState;
