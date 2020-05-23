50e2bd71a2900994dbecb54cf67626951c0ced5b
deliciousmonster <jaxon@deliciousmonster.com>
2020-05-23 13:02:09 -0600

    Update getUser.js

--- src/api/lms/getUser.js
+++ src/api/lms/getUser.js
@@ -1,8 +1,27 @@
 import queryLMS from '../queryLMS';
 
-export default async ({ email, pass }) =>
-  queryLMS({
+export default async ({ email, pass }) => {
+  const response = queryLMS({
     endpoint: 'getUser',
     method: 'POST',
     payload: { email, password: pass },
   });
+
+  if (response.error) {
+    return response;
+  }
+
+  if (!response.orgs) {
+    response.orgs = [
+      {
+        customer_id: response.customer_id,
+        customer_name: `${response.firstname}'s Org`,
+        status: 'owner',
+      },
+    ];
+  } else if (!Array.isArray(response.orgs)) {
+    response.orgs = [response.orgs];
+  }
+
+  return response;
+};
