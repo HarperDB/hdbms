7bd4f87cce27b09e05ccdc958c2c89b29cccece5
deliciousmonster <jaxon@deliciousmonster.com>
2020-06-19 17:33:10 -0600

    no cors

--- src/methods/examples/getPostManCollection.js
+++ src/methods/examples/getPostManCollection.js
@@ -2,7 +2,7 @@ import appState from '../../state/appState';
 import config from '../../../config';
 
 export default async () => {
-  const result = await fetch(config.postman_collection_url);
+  const result = await fetch(config.postman_collection_url, { mode: 'no-cors' });
   const newPostmanColection = await result.json();
 
   appState.update((s) => {
