1f9da6bb54b7e8cd09ebc9b3bcdcea352bdefb46
deliciousmonster <jaxon@deliciousmonster.com>
2020-07-30 15:02:25 -0600

    Update radioCheckbox.js

--- src/components/shared/radioCheckbox.js
+++ src/components/shared/radioCheckbox.js
@@ -1,60 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default class ThemedComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: [] };
-    this.handleClick = this.handleClick.bind(this);
-    this.setValue = this.setValue.bind(this);
-  }
+export default ({ required, onChange, options, type, defaultValue, ...rest }) => {
+  const [optionsArray, setOptionsArray] = useState([]);
+  const [value, setValue] = useState([]);
 
-  /* eslint-disable react/no-did-mount-set-state */
-  componentDidMount() {
-    const { defaultValue } = this.props;
+  const handleClick = (v) => {
+    const isRadio = type === 'radio';
+    let newValue = [...value];
+
+    if (newValue.indexOf(v) !== -1 && isRadio && required) {
+      console.log('not unsetting required radio');
+    } else if (newValue.indexOf(v) === -1 && isRadio) {
+      newValue = [v];
+    } else if (newValue.indexOf(v) === -1) {
+      newValue.push(v);
+    } else {
+      newValue.splice(newValue.indexOf(v), 1);
+    }
+    setValue(newValue);
+    onChange(!options.length || isRadio ? newValue[0] : newValue);
+  };
+
+  useEffect(() => {
     if (defaultValue) {
       const valueArray = !defaultValue.length ? [defaultValue] : defaultValue;
-      valueArray.map((v) => this.setValue(v.value));
+      valueArray.forEach((v) => handleClick(v.value));
     }
-  }
+  }, [defaultValue]);
 
-  setValue(v) {
-    let { value } = this.state;
-    const { required, onChange, options, type } = this.props;
-    const isRadio = type === 'radio';
-
-    if (value.indexOf(v) !== -1 && isRadio && required) {
-      console.log('not unsetting required radio');
-    } else if (value.indexOf(v) === -1 && isRadio) {
-      value = [v];
-    } else if (value.indexOf(v) === -1) {
-      value.push(v);
-    } else {
-      value.splice(value.indexOf(v), 1);
+  useEffect(() => {
+    if (options) {
+      setOptionsArray(!options.length ? [options] : options);
     }
-    this.setState({ value });
-    onChange(!options.length || isRadio ? value[0] : value);
-  }
+  }, [options]);
 
-  handleClick(v) {
-    this.setValue(v);
-  }
-
-  render() {
-    const { options, type, ...rest } = this.props;
-    const optionsArray = !options.length ? [options] : options;
-
-    return (
-      <div {...rest}>
-        {optionsArray &&
-          optionsArray.map((option) => (
-            <div key={`${type}${option.value}${option.label}`} onClick={() => this.handleClick(option.value)}>
-              <div className={`radio-checkbox ${this.state.value.indexOf(option.value) !== -1 ? 'show' : 'hidden'}`}>
-                {type === 'checkbox' ? <span className="checkmark">&#10004;</span> : <div className="dot" />}
-              </div>
-              <div className="radio-checkbox-label">{option.label}</div>
+  return (
+    <div {...rest}>
+      {optionsArray &&
+        optionsArray.map((option) => (
+          <div key={`${type}${option.value}${option.label}`} onClick={() => handleClick(option.value)}>
+            <div className={`radio-checkbox ${value.indexOf(option.value) !== -1 ? 'show' : 'hidden'}`}>
+              {type === 'checkbox' ? <span className="checkmark">&#10004;</span> : <div className="dot" />}
             </div>
-          ))}
-      </div>
-    );
-  }
-}
+            <div className="radio-checkbox-label">{option.label}</div>
+          </div>
+        ))}
+    </div>
+  );
+};
