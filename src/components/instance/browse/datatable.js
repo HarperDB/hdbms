c494877d9ce217cf3dc8390048ae8a9c55a12834
deliciousmonster <jaxon@deliciousmonster.com>
2020-08-04 12:38:49 -0600

    DEBOUNCE FILTER

--- src/components/instance/browse/datatable.js
+++ src/components/instance/browse/datatable.js
@@ -13,6 +13,7 @@ import DataTableHeader from './datatableHeader';
 import getTableData from '../../../methods/instance/getTableData';
 
 let controller;
+let debounceTimer;
 
 const DataTable = ({ tableState, setTableState, activeTable, defaultTableState }) => {
   const history = useHistory();
@@ -36,37 +37,40 @@ const DataTable = ({ tableState, setTableState, activeTable, defaultTableState }
   }, [schema, table]);
 
   useAsyncEffect(async () => {
-    if (controller) controller.abort();
-    if (canFetch) {
-      setLoading(true);
-      controller = new AbortController();
-      const { newData, newTotalPages, newTotalRecords, newSorted, newEntityAttributes, hashAttribute, dataTableColumns, error } = await getTableData({
-        schema,
-        table,
-        filtered: tableState.filtered,
-        pageSize: tableState.pageSize,
-        sorted: tableState.sorted,
-        page: tableState.page,
-        auth,
-        url,
-        signal: controller.signal,
-        is_local,
-        compute_stack_id,
-        customer_id,
-      });
+    if (debounceTimer) clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(async () => {
+      if (controller) controller.abort();
+      if (canFetch) {
+        setLoading(true);
+        controller = new AbortController();
+        const { newData, newTotalPages, newTotalRecords, newSorted, newEntityAttributes, hashAttribute, dataTableColumns, error } = await getTableData({
+          schema,
+          table,
+          filtered: tableState.filtered,
+          pageSize: tableState.pageSize,
+          sorted: tableState.sorted,
+          page: tableState.page,
+          auth,
+          url,
+          signal: controller.signal,
+          is_local,
+          compute_stack_id,
+          customer_id,
+        });
 
-      setTableState({
-        ...tableState,
-        tableData: newData,
-        totalPages: newTotalPages,
-        totalRecords: newTotalRecords,
-        sorted: newSorted,
-        newEntityAttributes,
-        hashAttribute,
-        dataTableColumns,
-        error,
-      });
-    }
+        setTableState({
+          ...tableState,
+          tableData: newData,
+          totalPages: newTotalPages,
+          totalRecords: newTotalRecords,
+          sorted: newSorted,
+          newEntityAttributes,
+          hashAttribute,
+          dataTableColumns,
+          error,
+        });
+      }
+    }, 100);
   }, [tableState.sorted, tableState.page, tableState.filtered, tableState.pageSize, tableState.lastUpdate, mounted]);
 
   useInterval(() => tableState.autoRefresh && setTableState({ ...tableState, lastUpdate: Date.now() }), config.refresh_content_interval);
