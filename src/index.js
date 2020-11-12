ceebf6222ed600b5020f38ca942f7dad6e2fced9
deliciousmonster <jaxon@deliciousmonster.com>
2020-11-12 15:30:23 -0700

    remove webVitals

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import stripePromise from './functions/stripe/stripePromise';
 import config from './config';
 import App from './components/app';
 import AlertTemplate from './components/shared/alert';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import './app.scss';
 
@@ -30,4 +30,4 @@ render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
