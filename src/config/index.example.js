0bf01217b66fe7fa7a8446928900ad79f95bb410
deliciousmonster <jaxon@deliciousmonster.com>
2020-12-11 17:25:54 -0700

    v2 addInstance

--- src/config/index.example.js
+++ src/config/index.example.js
@@ -1,5 +1,5 @@
 export default {
-  studio_version: '1.6.12',
+  studio_version: '1.6.13',
   env: 'dev',
   stripe_public_key: 'pk_test_XXXXXXXXXXXXXXXXXXXXXX',
   lms_api_url: 'XXXXXXXXXXXXXXXXXXXXXXXX',
