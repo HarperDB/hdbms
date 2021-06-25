6c016c504f544c4adec999a4ff8247190d809109
deliciousmonster <jaxon@deliciousmonster.com>
2021-06-25 15:40:54 -0600

    no us east 2

--- src/functions/api/lms/getRegions.js
+++ src/functions/api/lms/getRegions.js
@@ -15,7 +15,7 @@ export default async () => {
     let regions = [];
 
     if (Array.isArray(response)) {
-      regions = response;
+      regions = response.filter((r) => r.value === 'us-east-2');
     }
 
     return appState.update((s) => {
