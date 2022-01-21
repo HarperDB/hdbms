8674778f30d4a9e92377f7fdd85ba6326dbb0e73
deliciousmonster <jaxon@deliciousmonster.com>
2022-01-21 15:17:54 -0700

    package updates, change install URL

--- src/components/instances/new/Type.js
+++ src/components/instances/new/Type.js
@@ -22,53 +22,53 @@ function Type() {
 
   return (
     <Row>
-        <Col xs="12" lg="6" className="instance-form-card-holder">
-          <Card>
-            <CardBody className="instance-form-card-body">
-              <div className="text-bold text-center">Create New HarperDB Cloud Instance</div>
-              <hr />
-              <ul>
-                <li>Free License Tier Available!</li>
-                <li>Hosted on AWS</li>
-                <li>Managed by HarperDB</li>
-                <li>HarperDB License Included</li>
-                <li>24/7 Customer Support</li>
-                <li>Choose RAM and Disk Size</li>
-                <li>Scale On Demand</li>
-              </ul>
-              <hr />
-              <Button id="createCloudInstanceButton" className="mt-3" color="purple" block onClick={() => setFormData({ is_local: false })}>
-                Create HarperDB Cloud Instance
-              </Button>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xs="12" lg="6" className="instance-form-card-holder">
-          <Card>
-            <CardBody className="instance-form-card-body">
-              <div className="text-bold text-center">Register User-Installed Instance</div>
-              <hr />
-              <ul>
-                <li>Free License Tier Available!</li>
-                <li>
-                  <a href="https://harperdb.io/developers/get-started/" target="_blank" rel="noopener noreferrer">
-                    Click Here To Install HarperDB Yourself
-                  </a>
-                </li>
-                <li>Browse Instance Data</li>
-                <li>Configure Users, Roles, and Clustering</li>
-                <li>Manage Instance Licenses</li>
-                <li>Handle Version Upgrades</li>
-                <li>Instance Credentials Stay Local</li>
-              </ul>
-              <hr />
-              <Button id="createLocalInstanceButton" className="mt-3" color="purple" block onClick={() => setFormData({ is_local: true })}>
-                Register User-Installed Instance
-              </Button>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+      <Col xs="12" lg="6" className="instance-form-card-holder">
+        <Card>
+          <CardBody className="instance-form-card-body">
+            <div className="text-bold text-center">Create New HarperDB Cloud Instance</div>
+            <hr />
+            <ul>
+              <li>Free License Tier Available!</li>
+              <li>Hosted on AWS</li>
+              <li>Managed by HarperDB</li>
+              <li>HarperDB License Included</li>
+              <li>24/7 Customer Support</li>
+              <li>Choose RAM and Disk Size</li>
+              <li>Scale On Demand</li>
+            </ul>
+            <hr />
+            <Button id="createCloudInstanceButton" className="mt-3" color="purple" block onClick={() => setFormData({ is_local: false })}>
+              Create HarperDB Cloud Instance
+            </Button>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col xs="12" lg="6" className="instance-form-card-holder">
+        <Card>
+          <CardBody className="instance-form-card-body">
+            <div className="text-bold text-center">Register User-Installed Instance</div>
+            <hr />
+            <ul>
+              <li>Free License Tier Available!</li>
+              <li>
+                <a href="https://harperdb.io/docs/install-harperdb/" target="_blank" rel="noopener noreferrer">
+                  Click Here To Install HarperDB Yourself
+                </a>
+              </li>
+              <li>Browse Instance Data</li>
+              <li>Configure Users, Roles, and Clustering</li>
+              <li>Manage Instance Licenses</li>
+              <li>Handle Version Upgrades</li>
+              <li>Instance Credentials Stay Local</li>
+            </ul>
+            <hr />
+            <Button id="createLocalInstanceButton" className="mt-3" color="purple" block onClick={() => setFormData({ is_local: true })}>
+              Register User-Installed Instance
+            </Button>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
