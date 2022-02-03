589a341e3c4df2fda4ab47c083b8d36ac4f71a25
deliciousmonster <jaxon@deliciousmonster.com>
2022-02-03 12:29:32 -0700

    Update NewInstanceCard.js

--- src/components/instances/list/NewInstanceCard.js
+++ src/components/instances/list/NewInstanceCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router';
 import { Card, CardBody, Col } from 'reactstrap';
+import { useStoreState } from 'pullstate';
+import appState from '../../../functions/state/appState';
 
 function NewInstanceCard() {
   const history = useHistory();
   const { customer_id } = useParams();
+  const is_lumen = useStoreState(appState, (s) => s.is_lumen);
 
   const handleClick = () => {
     if (window._kmq) window._kmq.push(['record', 'clicked new instance card']);
@@ -15,7 +18,7 @@ function NewInstanceCard() {
     <Col xs="12" md="6" lg="4" xl="3" className="mb-4">
       <Card tabIndex="0" id="newInstanceCard" title="Add New Instance" className="instance new" onKeyDown={(e) => e.keyCode !== 13 || handleClick()} onClick={handleClick}>
         <CardBody className="d-flex flex-column align-items-center justify-content-center">
-          <span>Create New HarperDB Cloud Instance</span>
+          <span>Create New {is_lumen ? 'Lumen Edge HarperDB' : 'HarperDB Cloud'} Instance</span>
           <div className="my-4">
             <i className="fa fa-2x fa-plus-circle new-instance-plus" />
           </div>
