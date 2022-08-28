e53ffdc0230eddd234749ff4226607eb9dfdedad
deliciousmonster <jaxon@deliciousmonster.com>
2022-08-28 16:34:21 -0600

    install node_modules for CF deployment

---
+++ src/functions/api/instance/installNodeModules.js
@@ -0,0 +1,8 @@
+import queryInstance from '../queryInstance';
+
+export default async ({ auth, url, projects, dry_run = false }) =>
+  queryInstance({
+    operation: { operation: 'install_node_modules', projects, dry_run },
+    auth,
+    url,
+  });
--- src/functions/api/instance/packageCustomFunctionProject.js
+++ src/functions/api/instance/packageCustomFunctionProject.js
@@ -1,8 +1,8 @@
 import queryInstance from '../queryInstance';
 
-export default async ({ auth, url, project }) =>
+export default async ({ auth, url, project, skip_node_modules }) =>
   queryInstance({
-    operation: { operation: 'package_custom_function_project', project },
+    operation: { operation: 'package_custom_function_project', project, skip_node_modules },
     auth,
     url,
   });
