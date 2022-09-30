51b8c81c9c5c25bd574a259993dc204f4800bf87
deliciousmonster <jaxon@deliciousmonster.com>
2022-09-30 10:43:22 -0600

    update drivers

--- src/components/resources/drivers/index.js
+++ src/components/resources/drivers/index.js
@@ -16,7 +16,7 @@ function DriversIndex() {
               docs="powerbi"
               urls={[
                 {
-                  link: 'https://harperdb-drivers.s3.amazonaws.com/Power+BI+(Windows+exe)/setup.exe',
+                  link: 'https://harperdb-drivers.s3.amazonaws.com/Power+BI+Connector/setup.exe',
                 },
               ]}
             />
@@ -25,11 +25,12 @@ function DriversIndex() {
               name="Tableau"
               docs="tableau"
               urls={[
-                { label: 'Windows', link: 'https://harperdb-drivers.s3.amazonaws.com/Tableau/setup.exe' },
-                { label: 'Unix/Linux/Mac', link: 'https://harperdb-drivers.s3.amazonaws.com/Tableau/setup.jar' },
+                { label: 'Windows', link: 'https://harperdb-drivers.s3.amazonaws.com/Tableau+(Windows+Exe)/setup.exe' },
+                { label: 'Mac', link: 'https://harperdb-drivers.s3.amazonaws.com/Tableau+(Mac+Dmg)/setup.dmg' },
+                { label: 'Unix/Linux', link: 'https://harperdb-drivers.s3.amazonaws.com/Tableau+(Setup+Jar)/setup.jar' },
               ]}
             />
-            <DriverCard icon="file-excel-o" name="Excel (Windows exe)" docs="xls" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/Excel+(Windows+exe)/setup.exe' }]} />
+            <DriverCard icon="file-excel-o" name="Excel (Windows exe)" docs="xls" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/Excel+Add-In/setup.exe' }]} />
             <DriverCard
               icon="coffee"
               name="JDBC"
@@ -46,23 +47,16 @@ function DriversIndex() {
               icon="linux"
               name="ODBC Linux (RPM)"
               docs="odbc"
-              urls={[
-                { label: 'i686', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(RPM)/setup.i686.rpm' },
-                { label: 'x86_64', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(RPM)/setup.x86_64.rpm' },
-              ]}
+              urls={[{ label: 'x86_64', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(RPM)/setup.x86_64.rpm' }]}
             />
             <DriverCard
               icon="linux"
               name="ODBC Linux (DEB)"
               docs="odbc"
-              urls={[
-                { label: 'i686', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(DEB)/setup.i686.deb' },
-                { label: 'x86_64', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(DEB)/setup.x86_64.deb' },
-              ]}
+              urls={[{ label: 'x86_64', link: 'https://harperdb-drivers.s3.amazonaws.com/ODBC+Linux+(DEB)/setup.x86_64.deb' }]}
             />
-            <DriverCard icon="windows" name="ADO" docs="ado" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/ADO/setup.exe' }]} />
-            <DriverCard icon="windows" name="BizTalk" docs="bt" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/BizTalk/setup.exe' }]} />
-            <DriverCard icon="code" name="Cmdlets" docs="rcmd" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/Cmdlets/setup.exe' }]} />
+            <DriverCard icon="windows" name="ADO" docs="ado" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/ADO.NET+Driver/setup.exe' }]} />
+            <DriverCard icon="code" name="Cmdlets" docs="rcmd" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/Powershell+Cmdlets/setup.exe' }]} />
             <DriverCard icon="windows" name="SSIS" docs="rssis" urls={[{ link: 'https://harperdb-drivers.s3.amazonaws.com/SSIS/setup.exe' }]} />
           </Row>
         </CardBody>
