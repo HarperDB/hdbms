2db9bb337ba49ca3e96e3962abc3cc21756733be
Alex Ramsdell <alex@harperdb.io>
2023-04-11 16:03:29 -0400

    show static routes in custom functions page if version < 4.1

--- src/components/instance/functions/manage/index.js
+++ src/components/instance/functions/manage/index.js
@@ -14,12 +14,16 @@ import EmptyPrompt from '../../../shared/EmptyPrompt';
 import ErrorFallback from '../../../shared/ErrorFallback';
 import addError from '../../../../functions/api/lms/addError';
 import EntityReloader from './EntityReloader';
+import StaticEntityStatus from './StaticEntityStatus';
 import CopyableText from '../../../shared/CopyableText';
 
 function ManageIndex({ refreshCustomFunctions, loading }) {
   const { customer_id, compute_stack_id, action = 'edit', project, file } = useParams();
   const navigate = useNavigate();
   const custom_functions = useStoreState(instanceState, (s) => s.custom_functions);
+  const registration = useStoreState(instanceState, (s) => s.registration);
+  const [majorVersion, minorVersion] = (registration?.version || '').split('.');
+  const supportsStaticRoutes = parseFloat(`${majorVersion}.${minorVersion}`) < 4.1;
   const restarting = useStoreState(instanceState, (s) => s.restarting_service === 'custom_functions');
   const cf_server_url = useStoreState(instanceState, (s) => s.custom_functions_url || `${s.url.split(':').slice(0, -1).join(':')}:${s.custom_functions?.port}`);
   const baseUrl = `/o/${customer_id}/i/${compute_stack_id}/functions/${action}`;
@@ -42,7 +46,6 @@ function ManageIndex({ refreshCustomFunctions, loading }) {
     } else if (!hasProjects) {
       navigate(baseUrl);
     }
-    // NOTE: moved history object to navigate, make sense to track this as dep?
   }, [custom_functions?.endpoints, customer_id, compute_stack_id, navigate, action, project, file, baseUrl]);
 
   useInterval(async () => {
@@ -91,6 +94,11 @@ function ManageIndex({ refreshCustomFunctions, loading }) {
                 restarting={restarting}
                 showForm={action === 'edit'}
               />
+              { supportsStaticRoutes && <StaticEntityStatus
+                url={cf_server_url}
+                project={project}
+                fileCount={(project && custom_functions?.endpoints[project]?.static) || 0} /> }
+
             </>
           )}
           <hr className="mt-0" />
