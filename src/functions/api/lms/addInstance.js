71bbc04745fd22d93316610a7863c69d355e9e0b
deliciousmonster <jaxon@deliciousmonster.com>
2023-06-07 12:22:20 -0600

    un-de-structure props

--- src/functions/api/lms/addInstance.js
+++ src/functions/api/lms/addInstance.js
@@ -1,13 +1,28 @@
 import queryLMS from '../queryLMS';
 
-export default async ({ cloud_provider, auth, instance_region, ...rest }) =>
+export default async (props) =>
   queryLMS({
-    endpoint: cloud_provider === 'verizon' ? 'wl/addWavelengthInstance' : cloud_provider === 'akamai' ? '/addAkamaiInstance' : 'v2/addInstance',
+    endpoint: props.cloud_provider === 'verizon' ? 'wl/addWavelengthInstance' : props.cloud_provider === 'akamai' ? '/addAkamaiInstance' : 'v2/addInstance',
     method: 'POST',
-    auth,
+    auth: props.auth,
     payload: Object.entries({
-      user_id: auth.user_id,
-      wavelength_zone_id: instance_region,
-      ...rest,
+      user_id: props.auth.user_id,
+      customer_id: props.customer_id,
+      instance_name: props.instance_name,
+      is_local: props.is_local,
+      is_wavelength: props.is_wavelength,
+      is_akamai: props.is_akamai,
+      is_ssl: props.is_ssl,
+      host: props.host,
+      port: props.port,
+      instance_region: props.instance_region,
+      wavelength_zone_id: props.instance_region,
+      instance_type: props.instance_type,
+      stripe_plan_id: props.stripe_plan_id,
+      data_volume_size: props.data_volume_size,
+      stripe_storage_plan_id: props.stripe_storage_plan_id,
+      compute_subscription_id: props.compute_subscription_id,
+      storage_subscription_id: props.storage_subscription_id,
+      cloud_provider: props.cloud_provider,
     }).reduce((a, [k, v]) => (v == null ? a : ((a[k] = v), a)), {}),
   });
