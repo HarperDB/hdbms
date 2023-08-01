52db43108ec3c61d2616fecb52034f7c2b033ca0
Alex Ramsdell <alex@harperdb.io>
2023-08-01 19:39:56 -0400

    adjust html structure so a list contains a list always.

--- src/components/instance/functions/manage/FileBrowser.js
+++ src/components/instance/functions/manage/FileBrowser.js
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 
-function File({name}) {
-  return <li className="file">{name}</li>
+function Icon({ isDirectory=false }) {
+  return isDirectory ? <icon className="fas fa-folder-open" /> : null;
 }
 
-function FileTree({ directoryEntry }) {
+function File({ directoryEntry }) {
+  
+  const isDirectory = directoryEntry.entries;
+
+  return <li className="file">
+    <Icon isDirectory={isDirectory }/>
+    <span> { directoryEntry.name }</span>
+  </li>
+}
+
+function FileTree({ directoryEntry, isOpen }) {
   console.log('directoryEntry: ', directoryEntry);
 
   return (
-    <>
-      <File name={directoryEntry.name} />
+    <ul>
+      <File directoryEntry={directoryEntry} />
       {
         directoryEntry?.entries?.map(entry => (
-          <ul>
+          <ul className="folder folder-open">
             <FileTree directoryEntry={entry} />
           </ul>
         ))
       }
-    </>
+    </ul>
   )
 
 }
