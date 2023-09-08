6d59e0407c746133e5c645c8e7ac2616b106b325
Alex Ramsdell <alex@harperdb.io>
2023-09-08 08:53:52 -0400

    remove old rename components.

--- src/components/shared/webide/EditorWindow.js
+++ src/components/shared/webide/EditorWindow.js
@@ -56,82 +56,10 @@ export function NameFolderWindow({ active, onConfirm, onCancel }) {
   );
 }
 
-/*
-function RenameFileWindow() {
-  return (
-    <NameInput
-          label={`Please Choose a New Filename for '${selectedFile?.name}'`}
-          value={selectedFile?.name}
-          onBlur={()=>{}}
-          onConfirm={
-            (newName) => {
-              onFileRename(newName, selectedFile);
-            }
-          }
-          enabled={false}
-          onCancel={() => {}} />
-  );
-}
-
-function RenameFolderWindow() {
-  return (
-    <NameInput
-      label={`Please Choose a New Folder Name for 'this'`}
-      value={selectedFolder?.name}
-      onConfirm={ 
-        (newName) => {
-          onFolderRename(newName, selectedFolder)
-        }
-      }
-      onBlur={() => {}}
-      onCancel={() => {}} />
-  );
-}
-*/
-
-
-
-
 function EditorWindow({ children }) {
   return <Card className="editor-window">
   { children } 
   </Card>
 }
-
-function EditorWindowOld({ activeWindow, BlankWindow, CodeEditor, RenameFileWindow, RenameFolderWindow, /* RenameFile, RenameFolder, */ DeployWindow, NoFileSelected }) {
-
-  let CurrentWindow = null;
-
-  if (activeWindow === EDITOR_WINDOWS.BLANK_WINDOW) {
-    CurrentWindow = BlankWindow;
-  } else if (activeWindow === EDITOR_WINDOWS.CODE_EDITOR) {
-    CurrentWindow = CodeEditor;
-  } else if (activeWindow === EDITOR_WINDOWS.FILENAME_DIALOG) {
-    CurrentWindow = RenameFileWindow;
-    /*
-  } else if (activeWindow === EDITOR_WINDOWS.RENAME_FILE) {
-    CurrentWindow = RenameFileWindow;
-  } else if (activeWindow === EDITOR_WINDOWS.RENAME_FOLDER) {
-    CurrentWindow = RenameFolderWindow;
-    */
-  } else if (activeWindow === EDITOR_WINDOWS.NAME_FILE) {
-    CurrentWindow = NameFileWindow;
-  } else if (activeWindow === EDITOR_WINDOWS.NAME_FOLDER) {
-    CurrentWindow = NameFolderWindow;
-  } else if (activeWindow === EDITOR_WINDOWS.NO_FILE_SELECTED) {
-    CurrentWindow = NoFileSelectedWindow;
-  } else if (activeWindow === EDITOR_WINDOWS.DEPLOY) {
-    CurrentWindow = DeployWindow;
-  }
-
-  return (
-    <Card className="editor-window">
-    { <CurrentWindow /> }
-    </Card>
-  )
-
-}
-
-
 
 export default EditorWindow;
