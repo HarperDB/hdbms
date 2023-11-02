5467e1cc0983c1994e87184287a547120d6a8cb1
Alex Ramsdell <alex@harperdb.io>
2023-11-02 14:05:01 -0400

    lint.

--- src/components/shared/webide/windows/DeleteFileWindow.js
+++ src/components/shared/webide/windows/DeleteFileWindow.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, CardTitle, CardBody } from 'reactstrap';
 import ButtonWithLoader from '../../ButtonWithLoader'; 
 
