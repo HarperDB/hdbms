5b863fcbb9f7314aeadf8063142b7061d220d430
deliciousmonster <jaxon@deliciousmonster.com>
2024-04-09 13:44:10 -0600

    fix local studio loading stripe.

---
+++ src/components/layouts/LocalApp.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState, lazy, Suspense } from 'react';
+import { Route, Routes, Navigate, useNavigate, useLocation } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import usePersistedUser from '../../functions/state/persistedUser';
+import config from '../../config';
+
+import Loader from '../shared/Loader';
+import ErrorFallback from '../shared/ErrorFallback';
+import ErrorFallbackAuth from '../shared/ErrorFallbackAuth';
+
+import init from '../../functions/app/init';
+import changeFavIcon from '../../functions/app/changeFavIcon';
+import useInstanceAuth from '../../functions/state/instanceAuths';
+
+const TopNav = lazy(() => import(/* webpackChunkName: "topnav" */ '../TopNav'));
+const Instance = lazy(() => import(/* webpackChunkName: "instance" */ '../instance'));
+const SignIn = lazy(() => import(/* webpackChunkName: "signIn" */ '../auth/LocalSignIn'));
+
+let controller;
+
+function LocalApp() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [fetchingUser, setFetchingUser] = useState(true);
+  const [persistedUser, setPersistedUser] = usePersistedUser({});
+  const [instanceAuths] = useInstanceAuth({});
+
+  useEffect(() => {
+    changeFavIcon(persistedUser?.theme);
+  }, [persistedUser?.theme]);
+
+  useEffect(() => {
+    init({ currentPath: location.pathname, navigate, persistedUser, setPersistedUser, setFetchingUser, instanceAuths, controller });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div id="local-studio" className={`${persistedUser.theme}`}>
+      <div id="app-container">
+        <Suspense fallback={<Loader header=" " spinner />}>
+          <TopNav loggedIn={instanceAuths?.local?.valid} />
+        </Suspense>
+        {fetchingUser ? (
+          <Loader header="signing in" spinner />
+        ) : instanceAuths?.local?.valid ? (
+          <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <Suspense fallback={<Loader header=" " spinner />}>
+              {/* can we put instance routes in here, each in a suspense tag (since they're lazily loaded) */}
+              <Routes>
+                <Route element={<Instance />} path="/o/:customer_id/i/:compute_stack_id/*" />
+                <Route element={<Navigate to="/" replace />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        ) : (
+          <ErrorBoundary FallbackComponent={ErrorFallbackAuth}>
+            <Suspense fallback={<Loader header=" " spinner />}>
+              <Routes>
+                <Route element={<SignIn />} path="/" />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </div>
+      <div id="app-bg-color" />
+      <div className="version">HarperDB Local Studio v{config.studio_version}</div>
+    </div>
+  );
+}
+
+export default LocalApp;
--- src/functions/api/instance/readLog.js
+++ src/functions/api/instance/readLog.js
@@ -2,7 +2,6 @@ import queryInstance from '../queryInstance';
 import instanceState from '../../state/instanceState';
 
 export default async ({ auth, url, signal }) => {
-
   const logs = await queryInstance({
     operation: {
       operation: 'read_log',
@@ -15,7 +14,7 @@ export default async ({ auth, url, signal }) => {
   });
 
   return instanceState.update((s) => {
-    s.logs = logs.filter(log => log.message);
+    s.logs = logs.error ? [] : logs?.filter((log) => log.message) || [];
     s.logsError = false;
   });
 };
