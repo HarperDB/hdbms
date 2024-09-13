cef8b1c963ae7b904b5ec52de69b958a4de6d3c1
deliciousmonster <jaxon@deliciousmonster.com>
2024-09-12 18:45:28 -0600

    better application compatibility detection

--- src/components/shared/DataTableHeader.js
+++ src/components/shared/DataTableHeader.js
@@ -2,29 +2,32 @@ import React from 'react';
 import { Row, Col } from 'reactstrap';
 
 const DataTableHeader = ({ headerGroups, onSortedChange, sorted, showFilter }) =>
-  headerGroups.map((headerGroup) => (
-    <div {...headerGroup.getHeaderGroupProps()}>
-      <Row className="header g-0">
-        {headerGroup.headers.map((column) => (
-          <Col
-            key={column.id}
-            onClick={() => onSortedChange([{ id: column.id, desc: sorted[0]?.id === column.id ? !sorted[0]?.desc : false }])}
-            className={`${sorted[0]?.id === column.id ? 'sorted' : ''} ${sorted[0]?.desc ? 'desc' : 'asc'} ${column.id.indexOf('hdb-narrow') !== -1 ? 'action' : ''} px-1`}
-          >
-            <div className="text-renderer">{column.render('Header')}</div>
-          </Col>
-        ))}
-      </Row>
-      {showFilter && (
-        <Row className="filter g-0">
+  headerGroups.map((headerGroup) => {
+    const { key, ...rest } = headerGroup.getHeaderGroupProps();
+    return (
+      <div key={key} {...rest}>
+        <Row className="header g-0">
           {headerGroup.headers.map((column) => (
-            <Col key={column.id} className={column.id.indexOf('hdb-narrow') !== -1 ? 'action' : ''}>
-              {column.render('Filter')}
+            <Col
+              key={column.id}
+              onClick={() => onSortedChange([{ id: column.id, desc: sorted[0]?.id === column.id ? !sorted[0]?.desc : false }])}
+              className={`${sorted[0]?.id === column.id ? 'sorted' : ''} ${sorted[0]?.desc ? 'desc' : 'asc'} ${column.id.indexOf('hdb-narrow') !== -1 ? 'action' : ''} px-1`}
+            >
+              <div className="text-renderer">{column.render('Header')}</div>
             </Col>
           ))}
         </Row>
-      )}
-    </div>
-  ));
+        {showFilter && (
+          <Row className="filter g-0">
+            {headerGroup.headers.map((column) => (
+              <Col key={column.id} className={column.id.indexOf('hdb-narrow') !== -1 ? 'action' : ''}>
+                {column.render('Filter')}
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    );
+  });
 
 export default DataTableHeader;
