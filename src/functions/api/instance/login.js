0e13eff6bc71fd5e76922a4a31231c586a837242
Kris Zyp <kriszyp@gmail.com>
2024-10-06 21:59:39 -0600

    Add support for trying to log into servers and using cookies for maintaining authentication

---
+++ src/functions/api/instance/login.js
@@ -0,0 +1,9 @@
+import queryInstance from '../queryInstance';
+
+export default async ({ auth, url }) =>
+  queryInstance({
+    operation: { operation: 'login', username: auth.user, password: auth.pass },
+    auth,
+    url,
+    timeout: 5000,
+  });
