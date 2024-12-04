fad6a6ddb64e996f5db5a8d6bee03c7c48b43556
deliciousmonster <jaxon@deliciousmonster.com>
2024-12-04 03:08:55 -0500

    Update Free.js

--- src/components/instances/filter/Free.js
+++ src/components/instances/filter/Free.js
@@ -1,13 +1,12 @@
-import React, { useState, useCallback } from 'react';
-import { Button } from 'reactstrap';
+import React from 'react';
 
 function Free({ local }) {
-  return (
-    <div className="free-instances text-nowrap text-white">
-      <i className="fa fa-dollar-sign me-2" />
-      Free {true && <span className="ms-1">Enterprise</span>}
-    </div>
-  );
+	return (
+		<div className="free-instances text-nowrap text-white">
+			<i className="fa fa-dollar-sign me-2" />
+			Free {local && <span className="ms-1">Enterprise</span>}
+		</div>
+	);
 }
 
 export default Free;
