e44c6eb7053d9e64fd2738112a8243e273794899
Austin Akers <austin@harperdb.io>
2024-12-06 11:13:07 -0800

    returning undefined instead of null if there is no table key from description cache

--- src/functions/instance/state/describeTableCache.js
+++ src/functions/instance/state/describeTableCache.js
@@ -4,7 +4,7 @@ const getTableKey = (schema, table) => `${schema}/${table}`;
 
 const getTableDescriptionFromCache = (schema, table) => {
 	const tableKey = getTableKey(schema, table);
-	return descriptionCache.get(tableKey) ?? null;
+	return descriptionCache.get(tableKey) ?? undefined;
 };
 
 const setTableDescriptionInCache = (schema, table, description) => {
