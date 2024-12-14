7073665cc611ec9013d6d0ab5f290a7f6be68f4d
Austin Akers <austin@harperdb.io>
2024-12-13 20:25:48 -0800

    added getFabricUser function

---
+++ src/functions/api/lms/getFabricUser.js
@@ -0,0 +1,66 @@
+import queryLMS from '../queryLMS';
+import appState from '../../state/appState';
+import addError from './addError';
+import config from '../../../config';
+import queryFabric from '../functions/queryFabric';
+
+export default async ({ email, pass, loggingIn = false }) => {
+	let response = null;
+
+	try {
+		response = await queryFabric({
+			url: '/Login',
+			method: 'POST',
+			// TODO: headers might not be needed
+			headers: { 'Content-Type': 'application/json' },
+			body: {
+				email,
+				password: pass,
+			},
+		});
+
+		if (response.error && loggingIn) {
+			return appState.update((s) => {
+				s.auth = { ...response, time: Date.now() };
+			});
+		}
+
+		if (response.error) {
+			return false;
+		}
+
+		// if (!response.orgs) {
+		// 	response.orgs = [
+		// 		{
+		// 			customer_id: response.customer_id.toString(),
+		// 			customer_name: `${response.firstname}'s Org`,
+		// 			status: 'owner',
+		// 		},
+		// 	];
+		// } else if (!Array.isArray(response.orgs)) {
+		// 	response.orgs = [response.orgs];
+		// }
+
+		const newAuth = { email, ...response };
+		appState.update((s) => {
+			s.auth = newAuth;
+		});
+
+		return newAuth;
+	} catch (e) {
+		// if (!loggingIn) {
+		// 	addError({
+		// 		type: 'lms data',
+		// 		status: 'error',
+		// 		url: config.lms_api_url,
+		// 		operation: 'getUser',
+		// 		error: { catch: e.message },
+		// 	});
+		// }
+		console.error(e, 'getFabricUser error');
+
+		return appState.update((s) => {
+			s.auth = { error: true, message: e.message, time: Date.now() };
+		});
+	}
+};
