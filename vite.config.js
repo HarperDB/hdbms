05da849234e6c7ff363cf3fe87857bde4b7b708f
Austin Akers <austin@harperdb.io>
2024-12-16 11:17:59 -0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		https: {
+			cert: fs.readFileSync(process.env.SSL_CRT_FILE),
+			key: fs.readFileSync(process.env.SSL_KEY_FILE),
+		},
+	},
 });
