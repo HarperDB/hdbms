a12e11d9a9b7849234251130d41ec0b5dad4f74e
Austin Akers <austin@harperdb.io>
2025-01-22 13:10:34 -0800

    fixed small routing errors

--- src/routes.ts
+++ src/routes.ts
@@ -53,13 +53,13 @@ export default [
 				// default route/view when navigating to an instance
 				index('./components/instance/browse/index.tsx'), // Show first database and first table in the list by default
 				route('add-database', './components/instance/browse/AddDatabase.tsx'), // Modal
-				route(':/:databaseId/add-table', './components/instance/browse/AddTable.tsx'), // Modal
+				route('/:databaseId/add-table', './components/instance/browse/AddTable.tsx'), // Modal
 				route('/:databaseId/:tableId', './components/instance/browse/BrowseDataTable.tsx'), // Child-view inside index.tsx
 				route('/:databaseId/:tableId/add-record', './components/instance/browse/AddRecord.tsx'), // Modal
 				route('/:databaseId/:tableId/edit/:recordId', './components/instance/browse/EditRecord.tsx'), // Child-view
 			]),
 
-			...prefix('org/:orgId/clusters/:clusterId/instance/:instanceId/applications', [
+			...prefix('org/:orgId/clusters/:clusterId/instances/:instanceId/applications', [
 				index('./components/instance/browse/applications/index.tsx'), // Show List of applications/components
 				route('new', './components/instance/browse/applications/NewApplication.tsx'), // Modal
 				route('edit/:appId', './components/instance/browse/applications/EditApplication.tsx'), // Append folder/file path to url (deep linking)
