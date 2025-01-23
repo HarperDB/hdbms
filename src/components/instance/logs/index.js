d74538f93d8cff83c7e48db3d9eb6c937f88804b
Austin Akers <austin@harperdb.io>
2025-01-23 13:33:23 -0800

    create logs page

---
+++ src/components/instance/logs/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+import Logs from './Logs';
+
+function LogsIndex() {
+	return (
+		<Row id="config">
+			<Col lg="2" xs="12">
+				<h1>Logs Sidebar</h1>
+			</Col>
+			<Col lg="10" xs="12">
+				<Logs />
+			</Col>
+		</Row>
+	);
+}
+
+export default LogsIndex;
