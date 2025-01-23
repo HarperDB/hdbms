21e222c1f7f22fb65782bb8c4b75c74477686e41
Austin Akers <austin@harperdb.io>
2025-01-23 13:51:46 -0800

    parsed the logs and put them in a code preview tag

--- src/components/instance/logs/LogRow.js
+++ src/components/instance/logs/LogRow.js
@@ -12,8 +12,15 @@ function LogsRow({ level, timestamp, message, tags, thread }) {
 				<Col className="text-nowrap">{new Date(timestamp || null).toLocaleTimeString()}</Col>
 				<Col className="text-nowrap">{thread}</Col>
 				<Col className="text-nowrap">{tags?.join(', ')}</Col>
+				{
+					// TODO: Create a "more info" button to show the full log details with the message
+					// TODO: Move the message to a full logs details tray/modal
+				}
 				<Col xs="12" className="mt-1">
-					{isObject(message) && message.error ? message.error : JSON.stringify(message).slice(1, -1)}
+					{/* {isObject(message) && message.error ? message.error : JSON.stringify(message).slice(1, -1)} */}
+					<pre>
+						<code>{isObject(message) ? JSON.stringify(message) : message}</code>
+					</pre>
 				</Col>
 			</Row>
 		</div>
