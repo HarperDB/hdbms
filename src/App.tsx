8b4e6b5aa815b150ec21068881b9b810425fe237
Austin Akers <austin@harperdb.io>
2025-01-30 21:03:16 -0800

    tailwind is installed. colors vars are added. Dark/Light themes work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
 import FabricApp from "./FabricApp";
 
 function App() {
 
   console.log('version', import.meta.env.VITE_STUDIO_VERSION) 
-  console.log('REACT_APP_LOCALSTUDIO', import.meta.env.VITE_REACT_APP_LOCALSTUDIO) 
+  console.log('REACT_APP_LOCALSTUDIO', import.meta.env.VITE_REACT_APP_LOCALSTUDIO)
+
+  
   return (
     <>
       <FabricApp />
