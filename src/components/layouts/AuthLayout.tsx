1367b289329c1f10f56c4495f477b41013eccb7b
Austin Akers <austin@harperdb.io>
2025-01-31 14:27:11 -0800

    removing tailwind

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -2,33 +2,32 @@ import { Outlet } from 'react-router';
 
 function AuthLayout() {
 	return (
-		<div className="h-100 bg-white dark:bg-gray-500">
-			<div className="h-100">
-				<div className="p-5 auth-studio-info d-none d-sm-flex justify-content-center align-items-center">
-					<div className="auth-studio-info-container">
-						<h1 className="auth-title">HarperDB Studio</h1>
-						<span className="mb-4 auth-subtitle d-inline-block">Manage all your HarperDB instances.</span>
-						<ul className="auth-info-list-items">
+		<main className=''>
+			<div className=''>
+				<section className=''>
+					<div>
+						<h1>HarperDB Studio</h1>
+						<span>Manage all your HarperDB instances.</span>
+						<ul>
 							<li>
-								<h3 className="item-title">Manage All Instances</h3>
-								<span className="item-subtitle">Set access, cluster, monitor, and more.</span>
+								<h3>Manage All Instances</h3>
+								<span>Set access, cluster, monitor, and more.</span>
 							</li>
 							<li>
-								<h3 className="item-title">Embedded API Server</h3>
-								<span className="item-subtitle">HarperDB components give you unlimited application flexibility.</span>
+								<h3>Embedded API Server</h3>
+								<span>HarperDB components give you unlimited application flexibility.</span>
 							</li>
 							<li>
-								<h3 className="item-title">Fully Managed Cloud & 5G Instances</h3>
-								<span className="item-subtitle">Go from zero to code in minutes.</span>
+								<h3>Fully Managed Cloud & 5G Instances</h3>
+								<span>Go from zero to code in minutes.</span>
 							</li>
 							<li>
-								<h3 className="item-title">Deploy Anywhere</h3>
-								<div className="mt-3">
+								<h3>Deploy Anywhere</h3>
+								<div>
 									<a
 										href="https://hub.docker.com/r/harperdb/harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
-										className="deploy-subitem"
 									>
 										Docker
 									</a>
@@ -36,7 +35,6 @@ function AuthLayout() {
 										href="https://www.npmjs.com/package/harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
-										className="deploy-subitem"
 									>
 										npm
 									</a>
@@ -44,7 +42,6 @@ function AuthLayout() {
 										href="https://docs.harperdb.io/docs/deployments/install-harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
-										className="deploy-subitem"
 									>
 										all options
 									</a>
@@ -52,12 +49,12 @@ function AuthLayout() {
 							</li>
 						</ul>
 					</div>
-				</div>
-				<div className="p-5 d-flex justify-content-center align-items-center auth-form-container">
+				</section>
+				<section className=''>
 					<Outlet />
-				</div>
+				</section>
 			</div>
-		</div>
+		</main>
 	);
 }
 
