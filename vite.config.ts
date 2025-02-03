1455a2bed387b7223a15bdceb3084cbdb23d71e3
Austin Akers <austin@harperdb.io>
2025-02-03 14:53:48 -0800

    back to tailwind.....

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import { reactRouter } from '@react-router/dev/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [reactRouter()],
+	plugins: [reactRouter(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
