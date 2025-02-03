f1390d457a3aace55ea5bb107e27341c36793729
Austin Akers <austin@harperdb.io>
2025-02-03 14:42:22 -0800

    going crazy uninstalling chakra ui

--- src/features/auth/SignIn.tsx
+++ src/features/auth/SignIn.tsx
@@ -1,30 +1,27 @@
-import { Button, Input } from '@chakra-ui/react';
-import { Field } from '@/components/ui/field';
-
 function SignIn() {
   return (
     <div>
       <h1>Sign In</h1>
       <form>
-        <Field invalid label="Email" errorText="Invalid email">
-        <Input 
+        <label label="Email" errorText="Invalid email">
+        <input 
           id="email"
           name="email"  
           type="email"
           placeholder="Email"
           onChange={() => { console.log('email changed') }}
         />
-        </Field>
-        <Field invalid label="Password" errorText="Invalid password">
-        <Input 
+        </label>
+        <label label="Password" errorText="Invalid password">
+        <input 
           required
           id="password"
           name="password" 
           type="password" 
           placeholder="Password" 
         />
-        </Field>
-        <Button type="submit">Sign In</Button>
+        </label>
+        <button type="submit">Sign In</button>
       </form>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [reactRouter()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
