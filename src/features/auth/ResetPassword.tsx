ed691f6ddeb3d90da134b2746647f1cdebac6be6
Austin Akers <austin@harperdb.io>
2025-02-04 12:02:53 -0800

    created LocalSignIn and Reset Password pages

--- src/features/auth/ResetPassword.tsx
+++ src/features/auth/ResetPassword.tsx
@@ -1,7 +1,18 @@
 function ResetPassword() {
   return (
-    <div>
-      <h1>Reset Password</h1>
+    <div className="text-white w-sm">
+      <h1 className="text-3xl font-light">Reset Password</h1>
+      <form>
+        <div className="mt-4 mb-2">
+          <label className="block pb-2 text-sm" htmlFor="email">
+            Email
+          </label>
+          <input id="email" name="email" type="email" placeholder="jane.doe@harperdb.io" />
+        </div>
+        <button type="submit" className="w-full py-2 mt-6 text-sm rounded-full blue-gradient">
+          Send Reset Link
+        </button>
+      </form>
     </div>
   )
 }
