2244be75c06c3128cd27874d52d83aefe124fbfb
Austin Akers <austin@harperdb.io>
2025-02-03 21:57:11 -0800

    sign in page base ui done

--- src/features/layouts/AuthLayout.tsx
+++ src/features/layouts/AuthLayout.tsx
@@ -1,32 +1,39 @@
 import { Outlet } from 'react-router';
 
+function ListItem ({ title, children }: { title: string, children: React.ReactNode }) {
+	return (
+		<li className='list-image-(--checkmark-icon) mt-4'>
+			<h3 className='text-xl font-bold'>{title}</h3>
+			{children}
+		</li>
+	);
+}
+
+
+
 function AuthLayout() {
 	return (
-		<div>
-				<section>
+		<div className='grid h-screen grid-cols-1 md:grid-cols-2'>
+				<section className='items-center justify-center hidden text-white md:flex blue-pink-gradient'>
 					<div>
-						<h1 className='text-3xl font-bold underline'>HarperDB Studio</h1>
+						<h1 className='text-3xl font-bold'>HarperDB Studio</h1>
 						<span>Manage all your HarperDB instances.</span>
-						<ul>
-							<li>
-								<h3>Manage All Instances</h3>
-								<span>Set access, cluster, monitor, and more.</span>
-							</li>
-							<li>
-								<h3>Embedded API Server</h3>
-								<span>HarperDB components give you unlimited application flexibility.</span>
-							</li>
-							<li>
-								<h3>Fully Managed Cloud & 5G Instances</h3>
-								<span>Go from zero to code in minutes.</span>
-							</li>
-							<li>
-								<h3>Deploy Anywhere</h3>
-								<div>
+						<ul className='ps-5'>
+							<ListItem title='Manage All Instances'>
+								<span>View, create, and delete instances from one location.</span>
+							</ListItem>
+							<ListItem title='Embedded API Server'>
+								<span>HarperDB components give you unlimited application flexibility</span>
+							</ListItem>
+							
+							<ListItem title='Fully Managed Cloud & 5G Instances' children={<span>Go from zero to code in minutes.</span>} />
+							<ListItem title='Deploy Anywhere' children={
+								<div className='mt-6'>
 									<a
 										href="https://hub.docker.com/r/harperdb/harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
+										className='p-2 mr-2 border-2 border-blue-100 rounded-md'
 									>
 										Docker
 									</a>
@@ -34,6 +41,7 @@ function AuthLayout() {
 										href="https://www.npmjs.com/package/harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
+										className='p-2 mr-2 border-2 border-blue-100 rounded-md'
 									>
 										npm
 									</a>
@@ -41,17 +49,33 @@ function AuthLayout() {
 										href="https://docs.harperdb.io/docs/deployments/install-harperdb"
 										target="_blank"
 										rel="noreferrer noopener"
+										className='p-2 border-2 border-blue-100 rounded-md'
 									>
 										all options
 									</a>
 								</div>
-							</li>
+							} />
 						</ul>
 					</div>
 				</section>
-				<section>
+				<section className='flex items-center justify-center bg-purple-dark dark:bg-black-dark'>
 					<Outlet />
 				</section>
+				<button className='fixed p-2 text-white bg-blue-400 rounded-md bottom-4 right-4'
+					  onClick={() => {
+							document.documentElement.classList.toggle(
+								"dark"
+							);
+							localStorage.setItem(
+								"theme",
+								document.documentElement.classList.contains(
+									"dark"
+								)
+									? "dark"
+									: "light"
+							);
+						}}
+				>Toggle Theme</button>
 		</div>
 	);
 }
