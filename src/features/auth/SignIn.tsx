69ac6cdb37f2ea2bd2385ae76d05c34c099136a9
Austin Akers <austin@harperdb.io>
2025-02-03 21:57:11 -0800

    sign in page base ui done

--- src/features/auth/SignIn.tsx
+++ src/features/auth/SignIn.tsx
@@ -1,29 +1,33 @@
 function SignIn() {
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className="text-white">
+      <h1 className="text-3xl font-light">Sign in to HarperDB Studio</h1>
       <form>
-        <label>
+        <div className="py-4">
+        <label className="block pb-2 text-sm" htmlFor="email">
         Email
+        </label>
         <input 
           id="email"
           name="email"  
           type="email"
-          placeholder="Email"
+          placeholder="email address"
           onChange={() => { console.log('email changed') }}
         />
-        </label>
-        <label>
+        </div>
+        <div>
+        <label className="block pb-2 text-sm" htmlFor="password">
         Password
+        </label>
         <input 
           required
           id="password"
           name="password" 
           type="password" 
-          placeholder="Password" 
+          placeholder="password" 
         />
-        </label>
-        <button type="submit">Sign In</button>
+        </div>
+        <button type="submit" className="w-full py-2 mt-6 text-sm rounded-full blue-gradient">Sign In</button>
       </form>
     </div>
   )
