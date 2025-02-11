f332a5930f62fea4ce71e3e323ceb8baa73c0d5c
Austin Akers <austin@harperdb.io>
2025-02-11 15:34:59 -0800

    collapsable folders are possible in file explorer

--- src/components/instance/functions/manage/applicationsEditor/FileBrowser.js
+++ src/components/instance/functions/manage/applicationsEditor/FileBrowser.js
@@ -43,9 +43,15 @@ const isFolder = (entry) => Boolean(entry.entries);
 function ProjectIcon() {
 	return <i className={cn(`project-icon fas fa-file-code`)} />;
 }
-function FolderIcon({ toggleClosed }) {
-	// eslint-disable-next-line
-	return <i onClick={toggleClosed} onKeyDown={toggleClosed} className={cn(`folder-icon fas fa-folder-open`)} />;
+function FolderIcon({ toggleClosed, isOpen }) {
+	return (
+		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
+		<i
+			onClick={toggleClosed}
+			onKeyDown={toggleClosed}
+			className={cn(`folder-icon fas ${isOpen ? 'fa-folder-open' : 'fa-folder'}`)}
+		/>
+	);
 }
 
 function FiletypeIcon({ extension }) {
@@ -99,7 +105,6 @@ function File({ directoryEntry, selectedFile, selectedFolder, onFileSelect, onFo
 	const deployFileIconClass = 'deploy-project';
 	const isFileSelected = directoryEntry.path === selectedFile;
 	const isFolderSelected = directoryEntry.path === selectedFolder?.path;
-
 	// file receives open/close toggle func from
 	// parent. if it's a dir, calls toggle func on click
 	// if it's a flat file, calls onFileSelect so
@@ -119,7 +124,6 @@ function File({ directoryEntry, selectedFile, selectedFolder, onFileSelect, onFo
 		// that we don't untoggle directory selection; leave selected if icon clicked.
 		const iconWasClicked =
 			e.target.classList.contains(renameFileIconClass) || e.target.classList.contains(deployFileIconClass);
-
 		// if icon's clicked, select, but don't unselect.
 		// if (iconWasClicked) return;
 
@@ -188,7 +192,12 @@ function Folder({
 			{
 				// FIXME: don't hardcode 'components', get from root .name property of fileTree.
 				directoryEntry.name !== 'components' ? (
-					<li key={directoryEntry.key} className={cn('folder-container')}>
+					<li
+						key={directoryEntry.key}
+						className={cn(
+							`${directoryEntry.entries ? 'folder-container' : 'file-container'} ${open ? 'folder-open' : 'folder-closed'}`
+						)}
+					>
 						{directoryEntry.package ? (
 							<Package
 								selectedPackage={selectedPackage}
