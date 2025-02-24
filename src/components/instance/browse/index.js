f459ddf8d1ee641c5124c212adb6ec57df338f85
Austin Akers <austin@harperdb.io>
2025-02-24 12:16:05 -0800

    minor verbiage fix

--- src/components/instance/browse/index.js
+++ src/components/instance/browse/index.js
@@ -40,10 +40,10 @@ function NoPrimaryKeyMessage({ table }) {
 				<CardTitle>No Primary Key</CardTitle>
 				<i className="mt-3 fa fa-warning" />
 				<span className="mt-3">
-					The table {`'${table}'`} does not have a primary key. The Harper Studio does not currently support tables
-					without a primary key defined. Please see the{' '}
+					The table {`'${table}'`} does not have a primary key. Harper Studio does not currently support tables without
+					a primary key defined. Please see the{' '}
 					<a href="https://docs.harperdb.io/docs" target="_blank" rel="noreferrer">
-						Harper documention
+						Harper documentation
 					</a>{' '}
 					to see the standard Harper querying options.
 				</span>
