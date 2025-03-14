d1551fce10d7d8c4728ddd2e139fa73300e891ce
Austin Akers <austin@harperdb.io>
2025-03-14 00:14:57 -0700

    some css and a todo

--- src/features/clusters/ClustersList.tsx
+++ src/features/clusters/ClustersList.tsx
@@ -15,37 +15,42 @@ function ClustersList() {
 	return (
 		<div>
 			<section>
-				<div className="flex flex-col-reverse justify-between gap-4 mb-10 md:gap-0 md:flex-row">
-					<div>
-						<Input placeholder="Filter clusters by name" className="inline-block w-3/5 md:w-64" />
-						<Button className="inline-block w-2/5 md:w-auto md:ml-4">
-							Sort by A-Z
-							<span>
-								<ChevronDown className="inline-block" />
-							</span>
-						</Button>
+				{isSuccess && orgInfo?.clusters?.length ? (
+					<div className="flex flex-col-reverse justify-between gap-4 mb-10 md:gap-0 md:flex-row">
+						<div>
+							<Input placeholder="Filter clusters by name" className="inline-block w-3/5 md:w-64" />
+							<Button className="inline-block w-2/5 md:w-auto md:ml-4">
+								Sort by A-Z
+								<span>
+									<ChevronDown className="inline-block" />
+								</span>
+							</Button>
+						</div>
+						<NewClusterModal orgId={organizationId} />
 					</div>
-					<NewClusterModal orgId={organizationId} />
-				</div>
+				) : null}
 			</section>
 			<section>
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-12">
+				<>
 					{isSuccess && orgInfo?.clusters?.length ? (
-						orgInfo?.clusters.map((cluster) => (
-							<div key={cluster.id} className="cols-span-1 md:col-span-4 lg:col-span-3 2xl:col-span-2">
-								<ClusterCard
-									clusterName={cluster.name}
-									clusterId={cluster.id}
-									organizationId={cluster.organizationId}
-								/>
-							</div>
-						))
+						<div className="grid grid-cols-1 gap-4 md:grid-cols-12">
+							{orgInfo?.clusters.map((cluster) => (
+								<div key={cluster.id} className="cols-span-1 md:col-span-4 lg:col-span-3 2xl:col-span-2">
+									<ClusterCard
+										clusterName={cluster.name}
+										clusterId={cluster.id}
+										organizationId={cluster.organizationId}
+									/>
+								</div>
+							))}
+						</div>
 					) : (
-						<div className="cols-span-1 md:col-span-12">
+						<div className="flex-col space-y-5 items-center justify-center text-center">
 							<h2 className="text-2xl text-center text-white">No clusters found. Create a new cluster.</h2>
+							<NewClusterModal orgId={organizationId} />
 						</div>
 					)}
-				</div>
+				</>
 			</section>
 		</div>
 	);
