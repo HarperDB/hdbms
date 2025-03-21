2409ef38e7ac022669a57037ac9bf389ef173f70
Austin Akers <austin@harperdb.io>
2025-03-20 21:33:42 -0700

    changes to routing for clusters/list to /clusters, and more

--- src/features/clusters/index.tsx
+++ src/features/clusters/index.tsx
@@ -1,17 +1,7 @@
-import { Navigate, Outlet, getRouteApi, useMatchRoute } from '@tanstack/react-router';
-
-const route = getRouteApi('');
+import { Outlet } from '@tanstack/react-router';
 
 function ClustersIndex() {
-	const { organizationId } = route.useParams();
-	const matchRoute = useMatchRoute();
-	const match = matchRoute({ to: '/orgs/$organizationId/clusters' });
-	// TODO: Fix, back button doesn't work due to this redirect
-	if (match) {
-		return <Navigate to={`/orgs/${organizationId}/clusters/list`} />;
-	} else {
-		return <Outlet />;
-	}
+	return <Outlet />;
 }
 
 export default ClustersIndex;
